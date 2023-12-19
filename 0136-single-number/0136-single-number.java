8bb88f0023b320400532f31bad1678da0e082fbf
raghav20232023 <153320363+raghav20232023@users.noreply.github.com>
2023-12-20 02:36:48 +0530

    Time: 1 ms (99.94%), Space: 45 MB (22.64%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,11 @@
+import java.util.*; 
+
+class Solution {
+    public int singleNumber(int[] nums) {
+    int xorr = 0;
+    for(int  i = 0; i < nums.length; i++){
+        xorr = xorr^nums[i];
+    }
+    return xorr;
+    }
+}
