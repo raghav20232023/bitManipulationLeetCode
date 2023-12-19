e90e1694f6de149894150690fe2fc10449b0457f
raghav20232023 <153320363+raghav20232023@users.noreply.github.com>
2023-12-20 02:56:47 +0530

    Time: 2 ms (83.80%), Space: 46.3 MB (63.30%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -8,6 +8,11 @@ class Solution {
             while(temp != 0){ 
                 temp = temp & (temp-1);
                 count++;
+        //counting number of bits using brian kernighan's algorithm of counting number of 1 
+        //adding comments decreases runtime and increases memory consumption
+        //you can add comments here, but remember in competitive coding dont add comments
+        //adding comments here will not affect anything
+
             }
             arr[i] = count;
         }
